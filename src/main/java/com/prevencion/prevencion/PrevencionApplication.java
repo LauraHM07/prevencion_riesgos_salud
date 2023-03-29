d01cbfabf6da148f170a2cf4495e230e8a09f52a
Laura Haro <lauraharomolina@gmail.com>
2023-03-29 10:52:17 +0200

    Inicio

---
+++ src/main/java/com/prevencion/prevencion/PrevencionApplication.java
@@ -0,0 +1,13 @@
+package com.prevencion.prevencion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrevencionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrevencionApplication.class, args);
+	}
+
+}
